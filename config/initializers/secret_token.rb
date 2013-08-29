dbe8e23a501064fe8334f2ce878dc708be50733c
donneshaw <henryxiaojh@gmail.com>
2013-08-29 14:05:37 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RwAutoTest::Application.config.secret_token = '7293713a5c947118b1dcf2bf530411557a0d198e655ad7d0996e968182c8fcca64efa61cf52ce68ca532e1f86c970bcfabbb0ac435cca36e088bba3d2f1ab5a0'
